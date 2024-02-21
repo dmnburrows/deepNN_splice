0636fa6fcdb0204a0cc4841cd375a623cf4a0e66
dmnburrows <dominicburrows@hotmail.co.uk>
2024-02-20 17:24:43 -0800

    pytorch env hell

--- STAR_genome-generate.sh
+++ STAR_genome-generate.sh
@@ -2,8 +2,8 @@
 echo Starting
 STAR --runMode genomeGenerate \
      --runThreadN 12 \
-     --genomeDir /cndd3/dburrows/DATA/annotations/genome/grcm38.p3/STAR_genome \
-     --genomeFastaFiles /cndd3/dburrows/DATA/annotations/genome/grcm38.p3/GRCm38.p3.genome.fa \
-     --sjdbGTFfile /cndd3/dburrows/DATA/annotations/genome/grcm38.p3/gencode.vM3.chr_patch_hapl_scaff.annotation.gtf \
+     --genomeDir /cndd3/dburrows/DATA/annotations/genome/grcm38.p3/STAR_genome.1-19-X \
+     --genomeFastaFiles /cndd3/dburrows/DATA/annotations/genome/grcm38.p3/GRCm38.p3.genome_1-19-X.fa \
+     --sjdbGTFfile /cndd3/dburrows/DATA/annotations/genome/grcm38.p3/gencode.vM3.chr_1-19-X.annotation.gtf \
      --sjdbOverhang 50
 echo DoneS
